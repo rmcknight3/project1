4fd3d778c18afcaee3fa764e3f96b96b77aba2d5
Richard <rmcknight3@gmail.com>
2016-05-12 18:52:31 -0400

    project1

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Richard | Trouble</title>
+	</head>
+
+	<body>
+	<header>
+		<a href="index.php">
+			<h1>Richard</h1>
+			<h2>Trouble</h2>
+		</a>
+
+		<nav>
+			<ul>
+				<li><a href="index.php">Portfolio</a></li>
+				<li><a href="about.html">About</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</nav>
+
+	</header>
+
+		<section>
+			<ul>
+				<li></li>
+			</ul>
+		</section>
+
+		<footer>
+			<p>&copy; 2016 Richard</p>
+		</footer>
+	</body>
+</html>
